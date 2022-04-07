af779e4d433b64e454e89215d3549e51552514b8
Arjun Kalburgi <askalburgi@gmail.com>
2022-04-07 18:14:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import '@nylas/components-agenda'
+
+
+function App() {
+  return (
+    <main>
+<nylas-agenda id="6484c482-4eea-45fb-bd74-331756839f27" > </nylas-agenda>
+    </main>
+  );
+}
+
+export default App;
