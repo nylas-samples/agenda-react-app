bfbe362dc14cc3fe4a62cb622399e3440e348911
Arjun Kalburgi <askalburgi@gmail.com>
2022-04-08 15:08:41 +0000

    added some config options

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import '@nylas/components-agenda'
 function App() {
   return (
     <main>
-<nylas-agenda id="6484c482-4eea-45fb-bd74-331756839f27" > </nylas-agenda>
+<nylas-agenda 
+  id="6484c482-4eea-45fb-bd74-331756839f27" 
+          allow_date_change="true"
+        hide_ticks="false"> </nylas-agenda>
     </main>
   );
 }
